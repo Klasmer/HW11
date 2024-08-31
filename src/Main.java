f0e6033d61024b0639d75801276c2a489cade735
klasmer <klasmer495@gmail.com>
2024-08-31 18:11:43 +0300

    Методы. Задача 2.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,34 @@ public class Main {
         }
     }
 
+    public static void printClientOsDeviceYear(int clientOs, int clientDeviceYear) {
+        if (clientOs == 0)
+            if (clientDeviceYear < 2015) {
+                System.out.println("Устоновите облегченную версию приложения для iOS по ссылке ");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        else if (clientOs == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Устоновите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения Android по ссылке");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Задача 1");
         int year = 2105;
         printIsLeapYearResult(year);
 
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2012;
+        byte clientOs = 1;
+        printClientOsDeviceYear(clientOs, clientDeviceYear);
+
+
     }
+
 }
+
