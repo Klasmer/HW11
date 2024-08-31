a1b48886e9dcfbc115e06c1f6257ce1f9b8bdef3
klasmer <klasmer495@gmail.com>
2024-08-31 17:58:38 +0300

    Методы. Задача 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+    public static void printIsLeapYearResult(int year) {
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            System.out.println(year + " " + " год високосный");
+        } else {
+            System.out.println(year + " " + " не является високосный");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int year = 2105;
+        printIsLeapYearResult(year);
+
     }
 }
