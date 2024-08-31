d632a4bed5cb4960f0abb43605fa52a0cba7da29
klasmer <klasmer495@gmail.com>
2024-08-31 20:04:01 +0300

    Методы. Задача 3.

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,18 @@ public class Main {
         }
     }
 
-    public static void printClientOsDeviceYear(int clientOs, int clientDeviceYear) {
-        if (clientOs == 0)
+    public static void printClientIosDeviceYear(int clientOs, int clientDeviceYear) {
+        if (clientOs == 0) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Устоновите облегченную версию приложения для iOS по ссылке ");
             } else {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
-        else if (clientOs == 1) {
+        }
+    }
+
+    public static void printClientAndroidDeviceYear(int clientOs, int clientDeviceYear) {
+        if (clientOs == 1) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Устоновите облегченную версию приложения для Android по ссылке");
             } else {
@@ -43,9 +47,10 @@ public class Main {
         printIsLeapYearResult(year);
 
         System.out.println("Задача 2");
-        int clientDeviceYear = 2012;
-        byte clientOs = 1;
-        printClientOsDeviceYear(clientOs, clientDeviceYear);
+        int clientDeviceYear = 2016;
+        byte clientOs = 0;
+        printClientIosDeviceYear(clientOs, clientDeviceYear);
+        printClientAndroidDeviceYear(clientOs, clientDeviceYear);
 
         System.out.println("Задача 3");
         int deliveryDistance = 95;
